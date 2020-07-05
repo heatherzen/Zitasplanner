6792ce363d65d65ba4042478c37f66c33f5fc900
heatherzen <heather.zen@gmail.com>
2020-07-04 22:11:00 -0700

    tweaks to html/css and moment.js added and beginning of capturing and saving text-input

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,31 @@
-//write text in text-area
+//write text in text-area/check
 //capture text field values in local storage so that they stay on page after refresh
-//add moment.js to header for current day
+//add moment.js to header for current day/check
 //add event listener for button and capture data in local storage
 //color code block for past, present, future
 
-var currentDay = document.querySelector("#currentDay");
+var currentDay = document.getElementById("currentDay");
+var saveButton = document.getElementById("btn");
+var textInput = document.getElementById("input-event");
 
-function currentDay() {
-    const moment = require('moment');
-
-    let now = moment();
-    console.log(now.format('LL'));
-    
+function getCurrentDay() {
+    let now = moment().format('LL'); 
+    return(now);
 };
-currentDay();
+//end getCurrentDay fnct def
+
+var displayCurrentDay = function() {
+    $(currentDay).append(getCurrentDay);
+}
+//end of displayCurrentDay
+
+var saveTextInput = function(textInput) {
+    localStorage.setItem("textInput", textInput);
+    console.log(textInput);
+}
+
+
+getCurrentDay();
+displayCurrentDay();
+
+saveButton.addEventListener("click", saveTextInput);
